376a920cf93460e6b256d2e008e5529d3ef08831
zhaozhuang <zzzhaozzzz@163.com>
2021-11-15 18:13:13 +0800

    fix: 暂定提交

--- web_src/app.vue
+++ web_src/app.vue
@@ -1,16 +1,36 @@
+
 <script>
+import InputReg from "./components/inputReg";
+import { WebAssemblyFun } from "./server/c_bind";
+import { ref, reactive } from "vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      greeting: "Hello World!",
+  components: {
+    InputReg,
+  },
+  setup() {
+    const regValue = reactive({ value: "" });
+
+    const onregcontent = (value) => {
+      regValue.value = value;
     };
+    const onclick = async () => {
+      const { regParse, matchStr } = await WebAssemblyFun();
+      console.log(regParse);
+      let str = regParse(regValue.value);
+      console.log("格式化结果", JSON.parse(str));
+    };
+    return { onregcontent, onclick };
   },
 };
 </script>
 
 <template>
-  <p class="greeting">{{ greeting }}</p>
+  <div>
+    <InputReg @reg-content="onregcontent" :defValue="123"></InputReg>
+    <button @click="onclick">获取数据</button>
+  </div>
 </template>
 
 <style>
