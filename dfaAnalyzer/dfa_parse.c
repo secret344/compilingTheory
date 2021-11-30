f528ef7f323282bdbf6efbc65fe31f1b08644fb9
zhaozhuang <zzzhaozzzz@163.com>
2021-11-30 10:58:45 +0800

    fix:生成dfa节点集合

--- dfaAnalyzer/dfa_parse.c
+++ dfaAnalyzer/dfa_parse.c
@@ -1,22 +1,23 @@
 #include "dfa_parse.h"
 
 static NfaPair *nfaMachine = NULL;
-static Stack *dfaList = NULL;
+static SetRoot dfaList = NULL;
 
 void initDfaParse(NfaPair *nfaPair)
 {
     nfaMachine = nfaPair;
-    dfaList = new_stack();
+    dfaList = new_Set(Set_Struct);
 }
 
 void convertNfaToDfa()
 {
     int nextNum = STATE_FAILURE;
     Stack *dfaset = new_stack();
-    Stack *input = new_stack();
 
+    Stack *input = new_stack();
     sPointPush(input, nfaMachine->startNode);
     Stack *nfaStartClosure = e_closure(input);
+
     // 开始创建首个dfa节点
     Dfa *start = getDfaFromNfaSet(nfaStartClosure);
     sPointPush(dfaset, start);
@@ -24,19 +25,41 @@ void convertNfaToDfa()
     while (stacksize(dfaset))
     {
         Dfa *currentDfa = spop(dfaset);
-        sPointPush(dfaList, currentDfa);
+        addp_set(dfaList, currentDfa);
 
         for (size_t i = 0; i < ASCII_COUNT; i++)
         {
-            Stack *move = e_move(currentDfa->nfaStates, i);
+            Stack *currentState = new_stack();
+            set_To_Stack(currentState, currentDfa->nfaStates);
+            // 数据格式不同 转为相同格式
+            Stack *move = e_move(currentState, i);
             if (stacksize(move))
             {
                 Stack *closure = e_closure(move);
                 // 判断dfaList是否已经存在相应dfa节点
-                // TODO
+                Dfa *dfa = isNfaStatesExistInDfa(closure, dfaList);
+                if (dfa == NULL)
+                {
+                    // 不存在 新建dfa节点
+                    Dfa *newDfa = getDfaFromNfaSet(closure);
+                    addp_set(dfaList, newDfa);
+                    nextNum = newDfa->stateNum;
+                    // 加入栈 继续循环
+                    sPointPush(dfaset, newDfa);
+                }
+                else
+                {
+                    // 已经存在相应dfa
+                    nextNum = dfa->stateNum;
+                }
             }
             else
             {
+                nextNum = STATE_FAILURE;
+            }
+            if (nextNum != STATE_FAILURE)
+            {
+                printf("DFA from state: %d to state: %d on char: %zu", currentDfa->stateNum, nextNum, i);
             }
         }
     }
--- dfaAnalyzer/dfa_utils.h
+++ dfaAnalyzer/dfa_utils.h
@@ -4,5 +4,9 @@
 
 // 新建dfa节点
 // input: nfa集合(栈模拟)
-Dfa *getDfaFromNfaSet(Stack *input);
+extern Dfa *getDfaFromNfaSet(Stack *input);
+// 判断dfa集合是否已经存在closure相应的dfa节点
+extern Dfa *isNfaStatesExistInDfa(Stack *closure, SetRoot dfaList);
+// 临时的Set转为栈结构
+extern void set_To_Stack(Stack *target, SetRoot source);
 #endif
